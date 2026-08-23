6dc9cb80099ccf460947bc65efd220edbd7256da
RocketRile <riley_rhodes1@icloud.com>
2026-08-23 16:42:32 -0500

    Update fp_cam.gd

--- new-game-project/Players/fp_cam.gd
+++ new-game-project/Players/fp_cam.gd
@@ -3,7 +3,7 @@ extends Node3D
 var sensitivity = 0.2
 
 func  _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		get_parent().rotate_y(deg_to_rad(-event.relative.x * sensitivity))
